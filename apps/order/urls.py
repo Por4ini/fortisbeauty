36ebdc5ff3cf645bc9397ed21c173eee865e5f2d
fortisbeauty <fortisbeautygroup@gmail.com>
2022-12-30 14:04:47 +0000

    bag fix order

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
    ),
    path('create/',          views.CreateOrderView.as_view(), name='create'),
    path('payment/',         views.OrderPay.as_view(),    name='payment'),
+   path('prepayment/',      views.OrderPrePay.as_view(),    name='prepayment'),
    path('repeat/<id>/',     views.OrderRepeat.as_view(), name="repeat"),
    path('success/',         views.order_success,         name='success'),
    path('payment_response', views.payment_response,      name="payment_response")
--- apps/order/views/views__pay.py
+++ apps/order/views/views__pay.py
@@ -73,6 +73,65 @@ class OrderPay(APIView):
         return Response({'data': data,})
        
 
+class OrderPrePay(APIView):
+    def get_request_data(self, request_data, cart_data, order_id):
+        request_data = {
+            'merchantAccount': 'fortisbeauty_com_ua',
+            'merchantAuthType': 'SimpleSignature',
+            'merchantDomainName': 'fortisbeauty.store',
+            'merchantTransactionType': 'AUTO',
+            'merchantSignature': '',
+            'language': 'AUTO',
+            'returnUrl': 'https://fortisbeauty.store' + reverse('order:payment_response'),
+            'serviceUrl': 'https://fortisbeauty.store' + reverse('order:payment_response'),
+            'orderReference': int(time.time()) - 1622730000,
+            'orderDate': int(time.time()),
+            'amount': 200,
+            'currency': 'UAH',
+            'productName': [product['name'] for product in cart_data['items']],
+            'productPrice': [product['price'] for product in cart_data['items']],
+            'productCount': [product['quantity'] for product in cart_data['items']],
+            'deliveryList': [],
+            'clientFirstName': request_data['first_name'],
+            'clientLastName': request_data['last_name'],
+            'clientEmail': request_data['email'],
+            'clientPhone': request_data['phone'],
+        }
+        return request_data
+
+    def sign(self, data):
+        s = ';'.join([
+            data['merchantAccount'],
+            data['merchantDomainName'],
+            str(data['orderReference']),
+            str(data['orderDate']),
+            str(data['amount']),
+            data['currency'],
+        ])
+        for item in data['productName']:
+            s += ';' + str(item)
+        for item in data['productCount']:
+            s += ';' + str(item)
+        for item in data['productPrice']:
+            s += ';' + str(item)
+        return s
+
+    def post(self, request):
+        cart = Cart(request)
+        order = CreateOrder(data=request.data, user=request.user, cart=cart)
+        order = order.create_order()
+        data = self.get_request_data(request.data, cart.data(), order.id)
+        order.reference = int(data['orderReference'])
+        order.save()
+        sign = self.sign(data)
+        data['merchantSignature'] = hmac.new(
+            "d4501f69fdf17e81127e9a36f2cade9651439818".encode('utf-8'),
+            sign.encode('utf-8'), 'MD5'
+        ).hexdigest()
+        return Response({'data': data, })
+       
+
+
 
 @csrf_exempt
 def payment_response(request):
